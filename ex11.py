3f9308ab546180c14c8fd82e5ba11f3e98fb94f5
tanaphon <a.tanaphon@gmail.com>
2016-06-01 17:08:27 +0700

    add ex9-13

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print "How old are you?",
+age = int(raw_input())
+print "How tall are you?",
+height = int(raw_input())
+print "How much do you weight?",
+weight = int(raw_input())
+
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight)
+
